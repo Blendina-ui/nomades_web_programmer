cf24fa2c20ff5afaac0097a143188f457cba7433
Antonio Pisanello <antonio@apisanello.dev>
2024-06-18 12:56:24 +0200

    Add intro example JS

---
+++ example/cours/intro.js
@@ -0,0 +1,114 @@
+// function f(){
+//   var x = 12
+// }
+// let x = 23
+// console.log("console.log global", x)
+// f()
+// console.log("console.log global", x)
+// x = 34
+// console.log("console.log global", x)
+
+// let x
+// console.log(typeof x, x)
+
+// x = 12
+// console.log(typeof x, x)
+
+// let x
+// if(false) {
+//   x = 10
+// } else {
+//   x = 5
+// }
+
+// console.log(x)
+
+// let x = 5
+// x %= 2 // x = x+2
+// console.log(x)
+
+// console.log(x)
+
+// nom = "Pisanello"
+// prenom = "Antonio"
+// console.log(nom + " " + prenom)
+
+// let y = 10
+// console.log(x%y)
+
+// x = 0
+// x = x+1
+// x += 1
+// x++
+// console.log(x)
+
+// x = 0
+// console.log(x++)
+// x = 0
+// console.log(++x)
+
+// console.log(age >= 18)
+// console.log(!true)
+
+// let x = 2
+// // console.log(2 >= "2")
+
+
+// console.log(typeof x)
+
+// let number = -30
+// if (number > 0){
+//   console.log("Positive")
+// } else if (number < 0) {
+//   console.log("Negative")
+// } else if (number < 10) {
+//   console.log("< 10")
+// } else {
+//   console.log("Zero")
+// }
+
+// console.log("Always printed")
+
+// const boisson = "soda"
+// switch(boisson) {
+//   case "soda":
+//     console.log("Soda!")
+//     break
+//   case "cafe":
+//     console.log("cafe!")
+//     break
+//   case "the":
+//     console.log("the!")
+//     break
+//   default:
+//     console.log("Another drink")
+//     break
+// }
+
+// if(boisson == "soda") {
+//   console.log("Soda!")
+// } else if(boisson == "cafe") {
+//   console.log("cafe!")
+// } else if(boisson == "the") {
+//   console.log("the!")
+// } else {
+//   console.log("Another drink") 
+// }
+
+const age = 21
+// let majority
+
+// if (age >= 18) {
+//   majority = "Major"
+// } else {
+//   majority = "Minor"
+// }
+
+if (age >= 18) 
+  majority = "Major"
+else {
+  majority = "Minor"
+  console.log("COUCOU")
+}
+// const majority = (age >= 18 ? (age < 3 ? "Jeune adulte" : "Adulte" ) : "Minor")
+console.log(majority)
